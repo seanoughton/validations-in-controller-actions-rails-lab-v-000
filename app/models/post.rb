0b4c0644b8a3bcba79ae4ebd813a6784e0b0e10f
Sean Oughton <seanoughton@gmail.com>
2018-04-25 23:09:31 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
     message: "%{value} is not a valid category" }
-    validates :content, length: { minimum: 100 }
+  validates :content, length: { minimum: 100 }
 end
